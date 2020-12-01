3916b7e2f93c3e2997fccf18a795d4b9ad0121f8
Daniele Esposti <daniele.esposti@gmail.com>
2020-12-01 22:42:09 +0000

    example

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,9 +1,32 @@
 use neon::prelude::*;
+use serde_json::{Result, Value};
 
+fn untyped_example(mut cx: FunctionContext) -> JsResult<JsNull> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into serde_json::Value.
+    let v: Value = serde_json::from_str(data).unwrap();
+
+    // Access parts of the data by indexing with square brackets.
+    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+
+    Ok(cx.null())
+}
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
 register_module!(mut cx, {
-    cx.export_function("hello", hello)
+    cx.export_function("untyped_example", untyped_example);
+    cx.export_function("hello", hello);
+    Ok(())
 });
